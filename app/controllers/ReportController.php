f1c8e648b744f2ed2661043b87a471dc274b286e
lrnzn <lorenzenilon@gmail.com>
2026-05-01 13:34:10 +0800

    updated ReportController

--- app/controllers/ReportController.php
+++ app/controllers/ReportController.php
@@ -1,4 +1,32 @@
 <?php
-// app/controllers/ReportController.php
-// TODO: Add ReportController logic when functionality is implemented
-?>
+
+require_once __DIR__ . "/../models/Report.php";
+
+class ReportController
+{
+    private Report $report;
+
+    public function __construct()
+    {
+        $this->report = new Report();
+    }
+
+    public function index(): void
+    {
+        $month  = $_GET['month'] ?? '';
+        $year   = $_GET['year']  ?? '';
+
+        $stats          = $this->report->getStats($month, $year);
+        $mostBorrowed   = $this->report->getMostBorrowed(5, $month, $year);
+        $topBorrowers   = $this->report->getTopBorrowers(5, $month, $year);
+        $byGenre        = $this->report->getBorrowsByGenre();
+        $availableMonths = $this->report->getAvailableMonths();
+
+        $stats = array_merge(
+            ['total_transactions' => 0, 'total_books' => 0, 'total_borrowers' => 0, 'total_penalties' => 0],
+            $stats
+        );
+
+        require __DIR__ . "/../views/admin/reports.php";
+    }
+}
